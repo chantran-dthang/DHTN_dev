dcfb6e861bd3df629a197a95468dd8c9080c47e3
chantran-dthang <chantran-dthang@users.noreply.github.com>
2020-07-17 21:12:43 +0700

    adding .html and .js file

---
+++ javascript/practice.js
@@ -0,0 +1,8 @@
+function CalRetangle(){
+    var a=prompt("Input retangle length: ");
+    var b=prompt("Input retangle width :");
+    var P=(a+b)*2;
+    var S=a*b;
+    console.log("Primeter is: "+P);
+    console.log("Square is: "+S);
+}
