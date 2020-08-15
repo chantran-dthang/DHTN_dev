ebb110f3d21da81b15a7ce51c51e974b7ae6044a
chantran-dthang <chantran-dthang@users.noreply.github.com>
2020-08-15 10:09:33 +0700

    Revert "detach function"
    This reverts commit 1aae0cdb21f4c4d6577b5685b9729db38ba01220.

--- javascript/practice.js
+++ javascript/practice.js
@@ -1,9 +1,6 @@
-
-function CalRetangle()
-{
+function CalRetangle(){
     var length=Number(prompt("Input retangle length: "));
     var width=Number(prompt("Input retangle width :"));
-
     var S=length*width;
     var P= ( length + width)*2;
     
@@ -12,29 +9,3 @@ function CalRetangle()
     console.log("Primeter is: " +P);
     console.log("Square is: " + S);
 }
-function CallVar(a, b)
-{
-    a=Number(prompt("Input retangle length: "));
-    b=Number(prompt("Input retangle width :"));
-}
-function CalPrimeter(a, b)
-{
-    console.log("Primeter is: " + (a+b)*2);
-}
-function CalSquare(a, b)
-{
-    console.log("Square is: " + a*b);
-}
-
-function CalRetangle2()
-{
-    var length, width;
-    CallVar(length, width);
-    CalPrimeter(length, width);
-    CalSquare(length, width);
-    
-}
-
-console.log( new Date());
-
-
