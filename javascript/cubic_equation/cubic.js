327e300fdb19005a41ff35662ad40f74beae97d7
chantran-dthang <chantran-dthang@users.noreply.github.com>
2020-09-05 14:02:18 +0700

    check issue 3.2

--- javascript/cubic_equation/cubic.js
+++ javascript/cubic_equation/cubic.js
@@ -1,14 +1,13 @@
 function cubic(a, b, c, d)
 {
-    if(a==0) //neu a=0 dua ve giai phuong trinh bac 2
+    if(a==0) //if a=0, solves the quadratic equation 
     {
         quadratic(b, c, d);
     }
     else
     {
-        var A=b/a, B=c/a, C=d/a; // dua phuong trinh bac 3 ve dang x^3 + ax^2 + bx + c=0
-        var p=B-A*A/3, q=C+(2*Math.pow(A, 3)-9*A*B)/27; //dua phuong trinh ve dang t^3 + pt +q =0
-        // var root={u:0, v:0, i:0} 
+        var A=b/a, B=c/a, C=d/a; // put the equation in form of x^3 + ax^2 + bx + c=0
+        var p=B-A*A/3, q=C+(2*Math.pow(A, 3)-9*A*B)/27; //put the equation form of t^3 + pt +q =0
         var roots;
         var delta=Math.pow(q, 2)/4+Math.pow(p, 3)/27;
         if (p==0) 
@@ -22,17 +21,15 @@ function cubic(a, b, c, d)
         else 
         {
             var delta=Math.pow(q, 2)/4 + Math.pow(p, 3)/27;
-            if (delta==0) // hai nghiem
+            if (delta==0) // two roots
             {      
-                // roots = [-2*Math.pow(q/2, 1/3), Math.pow(q/2, 1/3)];
                 roots = [-2*cuberoot(q/2), cuberoot(q/2)];
             } 
-            else if (delta > 0) // 1 nghiem
+            else if (delta > 0) // one root
             {            
-                // roots = [-Math.pow(q/2 + Math.sqrt(delta), 1/3)-Math.pow(q/2 - Math.sqrt(delta), 1/3)];
                 roots = [-1*cuberoot(q/2 + Math.sqrt(delta))-cuberoot(q/2 - Math.sqrt(delta))];
             }
-            else //3 nghiem, nhung co nghiem phuc
+            else //three roots includes complex root(s)
             {                        
                 var u = 2*Math.sqrt(-p/3);
                 var t = Math.acos(3*q/p/u)/3;  
@@ -44,7 +41,6 @@ function cubic(a, b, c, d)
         for (var i = 0; i < roots.length; i++)
         {
             roots[i] -= A/3;
-           // console.log(roots[i]);
         }
         console.log(roots);
         return roots;
